930aa312eb9269184d45b99b84d743a3022d1410
jeenadiksha <132073521+jeenadiksha@users.noreply.github.com>
2023-12-23 02:33:36 -0800

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,9 @@
+tpl = (1,2,3,4,5,6,7,8)
+n = len(tpl)
+half = n // 2
+
+if(n == sorted()):
+    print("The first half of the tuple is sorted ")
+else:
+    print("The first half of the tuple is not sorted ")
+
